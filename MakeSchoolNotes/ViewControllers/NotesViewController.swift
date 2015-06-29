f3e971566178a259b8843b4e15e240db4a2db6f7
Petrina Koh <petrinakoh21@gmail.com>
2015-06-29 14:01:19 -0700

    completed notes app

--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
-import Realm
+import RealmSwift
 
 class NotesViewController: UIViewController {
+    
 
+    @IBOutlet weak var tableView: UITableView!
+
+    var notes: Results<Note>! {
+        didSet {
+            // whenever notes update, update the table view
+            tableView?.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myNote = Note()
+        myNote.title = "Super Simple Test Note"
+        myNote.content = "A long piece of content"
+        
+        let realm = Realm() // 1
+        realm.write() { // 2
+            realm.add(myNote) // 3
+        }
+        
+        notes = realm.objects(Note)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
+
+extension NotesViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NoteCell", forIndexPath: indexPath) as! NoteTableViewCell //1
+        
+        let row = indexPath.row
+        let note = notes[row] as Note
+        cell.note = note
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int(notes?.count ?? 0)
+    }
 
 }
+
+
+
+
 
